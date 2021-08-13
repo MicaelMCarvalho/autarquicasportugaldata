3d6015d0b32cae349ac514f44dec1561b03f8b48
Micael Carvalho <micael.l.m.c@gmail.com>
2021-08-13 21:39:41 +0200

    Set up main to call  scrapper, working for 2017

--- scrapper/scrapper_last_years.py
+++ scrapper/scrapper_last_years.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 """
-Entry point for scrapper module
+Entry point for scrapper module to be used in 2017 and 2013
 in this module it will be defined all the logic behind the data scrapping from the website(s)
 """
 import requests
@@ -8,11 +8,15 @@ import json
 
        
 class scrapper:
+   def __init__(self, year, url):
+      self.year = year
+      self.url = url
+
    def iterateUrl(self):
       dictAllInfo = {"candidate":[]}
 
       for link in range(1, 29):
-         response = requests.get("https://www.eleicoes.mai.gov.pt/autarquicas2013/static-data/candidates/PARTIES-CANDIDATES-CM-PAGE-%s.json" % (link)) 
+         response = requests.get(self.url % (link)) 
          data = response.json()
          maxPageNum = data['numberOfPages'] #TODO: Lazy set range
          for candidate in data['electionCandidates']:
@@ -20,12 +24,5 @@ class scrapper:
             dictAllInfo['candidate'].append(candidate)
             #print(candidate['parentTerritoryName'])#candidates_obj['parentTerritoryName'], candidates_obj['territoryName'] , candidates_obj['candidates'] )
       
-         with open('all_candidates.json','w',encoding='utf-8') as file:
+         with open('all_candidates_%s.json'%(self.year),'w',encoding='utf-8') as file:
             json.dump(dictAllInfo,file,indent=2,ensure_ascii=False)
-            
-
-def main():
-   scrap = scrapper()
-   scrap.iterateUrl()    
-
-main()    
