d7538eff0ddbfa3f7ce7be9d96f433a93aa485d7
WeirdCockatoo <weirdcockatoo@localhost.localdomain>
2021-08-14 05:19:31 +0100

    :clown_face: small refactor and added fetch candidates by conselho

--- mining/mining.py
+++ mining/mining.py
@@ -4,3 +4,12 @@ Entry Point for mining module.
 In this module it will be defined all the logic behing, data analysis and results
 
 """
+
+class Mining:
+    def __init__(self,conselho,candidate):
+        self.conselho = conselho
+        self.candidate = candidate
+
+
+    def get_candidate_by_conselho(self):
+        return[ i for i in self.candidate['candidate'] if i.get('parentTerritoryName') == self.conselho]
